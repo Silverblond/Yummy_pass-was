07efe18ff951799d3d6430a967d0c75ba22d3c97
subin <han04245@naver.com>
2025-09-10 17:23:33 +0900

    Initial commit

---
+++ src/main/java/yuseteam/mealticketsystemwas/MealTicketSystemWasApplication.java
@@ -0,0 +1,13 @@
+package yuseteam.mealticketsystemwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealTicketSystemWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealTicketSystemWasApplication.class, args);
+    }
+
+}
